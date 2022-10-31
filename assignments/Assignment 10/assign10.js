984f91060e35cb2a458417f824a812c30f6e2b6a
mada2315 <mada2315@colorado.edu>
2022-10-30 22:02:27 -0600

    push

--- assignments/Assignment 10/assign10.js
+++ assignments/Assignment 10/assign10.js
@@ -1,81 +1,138 @@
-let employees = [
+/* Number One */
+let employees = {
+  "employeeInfo": [
     {
     "firstName": "Sam",
     "department": "Tech",
     "designation": "Manager",
     "salary": 40000,
-    "raise eligible": "true",
-    "wfh": "true"
+    "raise eligible": true
     },
     {
     "firstName": "Mary",
     "department": "Finance",
     "designation": "Trainee",
     "salary": 18500,
-    "raise eligible": "true",
-    "wfh": "false"
+    "raise eligible": true
     },
     {
     "firstName": "Bill",
     "department": "HR",
     "designation": "Executive",
     "salary": 21200,
-    "raise eligible": "false",
-    "wfh": "false"
+    "raise eligible": false
     },
-    {
-    "firstName": "Anna",
-    "department": "Tech",
-    "designation": "Executive",
-    "salary": 25600,
-    "raise eligible": "false",
-    "wfh": "true"
-    }
   ]
-
-var totalSalary = 0,
-salary = employees.salary, 
-    i;
-for (i = 0; i < salary.length; i++) { 
-    totalSalary += salary[i].amount; 
 }
 
-console.log(totalSalary);
+console.log(employees);
 
- let company = [
-    {
+/* Number Two */
+ let company = {
     "companyName": "Tech Stars",
     "website": "www.techstars.site",
-    "employees":[
+    "employeeInfo":[
         {
         "firstName": "Sam",
         "department": "Tech",
         "designation": "Manager",
         "salary": 40000,
-        "raise eligible": "true"
+        "raise eligible": true
         },
         {
         "firstName": "Mary",
         "department": "Finance",
         "designation": "Trainee",
         "salary": 18500,
-        "raise eligible": "true"
+        "raise eligible": true
         },
         {
         "firstName": "Bill",
         "department": "HR",
         "designation": "Executive",
         "salary": 21200,
-        "raise eligible": "false"
-        },
-        {
-        "firstName": "Anna",
-        "department": "Tech",
-        "designation": "Executive",
-        "salary": 25600,
-        "raise eligible": "false"
+        "raise eligible": false
         }
       ]
     }
- ]
-  
+ console.log(company);
+
+ /* Number Three */
+ let employeespt2 = {
+  "companyName": "Tech Stars",
+    "website": "www.techstars.site",
+    "employeeInfo":[
+        {
+        "firstName": "Sam",
+        "department": "Tech",
+        "designation": "Manager",
+        "salary": 40000,
+        "raise eligible": true
+        },
+        {
+        "firstName": "Mary",
+        "department": "Finance",
+        "designation": "Trainee",
+        "salary": 18500,
+        "raise eligible": true
+        },
+        {
+        "firstName": "Bill",
+        "department": "HR",
+        "designation": "Executive",
+        "salary": 21200,
+        "raise eligible": false
+        }
+      ]
+    }
+console.log(employeespt2);
+
+function addEmployee(obj, name, department, designation, salary, raise){
+  let employee = {
+    "firstName": name,
+    "department": department,
+    "designation": designation,
+    "salary": salary,
+    "raiseEligible": raise
+  };
+  obj['employeeInfo'].push(employee);
+}
+ addEmployee(employeespt2, "Anna", "Tech", "Executive", 25600, false);
+ 
+ /* Number Four */
+ let totalSalary = 0;
+ for (let i = 0; i < employeespt2.employeeInfo.length; i++){
+  totalSalary += employeespt2.employeeInfo[i]['salary'];
+ }
+
+ console.log(totalSalary);
+
+ /* Number Five */
+ for(let i = 0; i < employeespt2.employeeInfo.length; i++){
+  if(employeespt2.employeeInfo[i]['raiseEligible'] === true){
+    employeespt2.employeeInfo[i]['salary'] += employeespt2.employeeInfo[i]['salary'] * 0.1;
+    employeespt2.employeeInfo[i]['raiseEligible'] = false;
+  }
+ }
+
+ console.log(employeespt2.employeeInfo);
+
+ /* Number Six */
+ let wfh = ['Anna','Sam'];
+ for (let i = 0; i < employeespt2.employeeInfo.length; i++){
+  let check = false;
+  let name = employeespt2.employeeInfo[i].firstName;
+  console.log(check);
+  for (let j = 0; j < wfh.length; j++){
+    if(wfh[j] === name){
+      check = true;
+    }
+  }
+  if(check === true){
+    employeespt2['employeeInfo'][i].workFromHome = true;
+  }
+  else {
+    employeespt2['employeeInfo'][i].workFromHome = false;
+  }
+ }
+ console.log(employeespt2);
